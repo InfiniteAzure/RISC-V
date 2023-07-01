3db97d784586fbf8b2ebfb64973f590e77a47130
InfiniteAzure <119796695+InfiniteAzure@users.noreply.github.com>
2023-07-02 02:30:28 +0800

    added shuffle

--- main.cpp
+++ main.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
+#include <algorithm>
 #include "processor.h"
 #include "Thomaslo.h"
 
 int main() {
-    //freopen("../sample.data","r",stdin);
+    //freopen("../sample.data", "r", stdin);
+    //freopen("../ans.out","w",stdout);
     read_all();
     int Clo = 0;
+    int rand[3] = {0, 1, 2};
     Tomasulo t;
     while (true) {
         Clo++;
-        t.IFetch();
-        t.calculate();
-        t.commit();
-        t.commit();
-        t.commit();
+        std::random_shuffle(rand, rand + 3);
+        for (int i = 0; i < 3; i++) {
+            if (rand[i] == 0) {
+                t.commit();
+            }
+            if (rand[i] == 1) {
+                t.calculate();
+            }
+            if (rand[i] == 2) {
+                t.IFetch();
+            }
+        }
     }
 }
